e4d3b69051495e5cedd2661a9247345fa498f29f
Jesús Cazalilla <jcazalilla@gmail.com>
2025-01-19 20:15:04 +0100

    Inicio Actividad 3 PMDM

--- app/src/main/java/moreno/cazalilla/jesusmaria/MainActivity.java
+++ app/src/main/java/moreno/cazalilla/jesusmaria/MainActivity.java
@@ -2,14 +2,14 @@ package moreno.cazalilla.jesusmaria;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -19,6 +19,7 @@ import moreno.cazalilla.jesusmaria.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    NavController navController = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +27,37 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.btnCerrarSesion.setOnClickListener(this::logOutSession);
+        // binding.btnCerrarSesion.setOnClickListener(this::logOutSession);
 
-        binding.btnGarbarDatos.setOnClickListener(this::saveDataToFirestore);
+        //binding.btnGarbarDatos.setOnClickListener(this::saveDataToFirestore);
+
+
+        //navcontroller
+        Fragment navHostFragment = getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
+        if (navHostFragment != null) {
+            navController = NavHostFragment.findNavController(navHostFragment);
+            NavigationUI.setupWithNavController(binding.navView, navController);
+            NavigationUI.setupActionBarWithNavController(this, navController);
+        }
+
+        binding.navView.setOnItemSelectedListener(this::selectedBottonMenu);
 
     }
 
+    private boolean selectedBottonMenu(MenuItem menuItem) {
+        if (menuItem.getItemId() == R.id.nav_pokemons)
+            navController.navigate(R.id.navigation_pokemons);
+        else
+        if (menuItem.getItemId() == R.id.nav_pokedex)
+            navController.navigate(R.id.navigation_pokedex);
+        else
+            navController.navigate(R.id.navigation_ajustes);
+        return true;
+    }
+
+
+
+  /*  //datbase firestore
     private void saveDataToFirestore(View view) {
         PlayerData playerData = new PlayerData(binding.nameEdittext.getText().toString(),
                 binding.descriptionEdittext.getText().toString());
@@ -46,8 +72,9 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(this, "Error al guardar registro.", Toast.LENGTH_SHORT).show()
                 );
     }
+*/
 
-
+    // session login
     private void logOutSession(View view) {
         AuthUI.getInstance()
                 .signOut(this)
@@ -59,4 +86,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+
 }
